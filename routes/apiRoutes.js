a9d37765a1716c121b3f31c9d5387e84e771f1cc
ejackson1228 <ejackson1228@gmail.com>
2022-09-23 15:02:20 -0400

    added validation for createNewNote and added note ID GET route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ const { notes } = require('../db/db.json')
 const router = require('express').Router()
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
-const createNewNote = require('../lib/notes')
+const { createNewNote, findById, validateNote } = require('../lib/notes')
 
 
 router.get('/notes', (req, res) => { // get the notes objects as a response
@@ -10,12 +10,25 @@ router.get('/notes', (req, res) => { // get the notes objects as a response
     res.send(rFile);
 })
 
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    };
+});
+
 router.post('/notes', (req, res) => { //post request for new note
     req.body.id = uuidv4();
 
+    if (!validateNote)  {
+        res.status(400).send("This note is not formatted properly!");
+    } else {
     const note = createNewNote(req.body, notes);
 
-    res.json(note)
+    res.json(note);
+    }
 });
 
 module.exports = router;
