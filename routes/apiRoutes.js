d76064d59c0928ff3659ab4e1a4386caf30565d9
ejackson1228 <ejackson1228@gmail.com>
2022-09-25 14:52:38 -0400

    delete route close to working. need to work out json keeping deleted objects bug

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,6 +11,7 @@ router.get('/notes', (req, res) => { // get the notes objects as a response
 })
 
 router.get('/notes/:id', (req, res) => {
+    // notes = fs.readFileSync('./db/db.json');
     const result = findById(req.params.id, notes);
     if (result) {
         res.json(result);
@@ -19,13 +20,8 @@ router.get('/notes/:id', (req, res) => {
     };
 });
 
-router.delete('/notes/:id', (req, res) => { // <<< :id is established in deleteNote() @ index.js line 45
-    deleteNote(req.body.id, notes);
-    
-    return res.json('Delete Confirmed');
-})
-
 router.post('/notes', (req, res) => { //post request for new note
+    fs.readFileSync('./db/db.json');
     req.body.id = uuidv4();
 
     if (!validateNote(req.body))  {
@@ -35,6 +31,21 @@ router.post('/notes', (req, res) => { //post request for new note
 
     res.json(note);
     }
+});
+
+router.delete('/notes/:id', (req, res) => { // <<< :id is established in deleteNote() @ index.js line 45
+    if (req.params.id) {
+    var removeNote = req.params.id;
+    } else {
+        return res.status(404).send("Error: Note not found!")
+    }
+    var data = fs.readFileSync('./db/db.json', {encoding: 'utf-8'});
+    
+    var json = JSON.parse(data);
+    var savedNotes = json.notes;
+    json.notes = savedNotes.filter((note) => { return note.id !== removeNote});
+    fs.writeFileSync('./db/db.json', JSON.stringify(json));
+    return res.status(200).send(`Note Deleted!`);
 });
 
 module.exports = router;
