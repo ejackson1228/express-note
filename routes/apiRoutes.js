1bf2c1bf825d176b30d9c48e134fa98e0bdd4d46
ejackson1228 <ejackson1228@gmail.com>
2022-09-25 15:26:28 -0400

    internal server error with delete (500). trying again later.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,8 +11,8 @@ router.get('/notes', (req, res) => { // get the notes objects as a response
 })
 
 router.get('/notes/:id', (req, res) => {
-    // notes = fs.readFileSync('./db/db.json');
-    const result = findById(req.params.id, notes);
+    notesArray = fs.readFileSync('./db/db.json');
+    const result = findById(req.params.id, notesArray);
     if (result) {
         res.json(result);
     } else {
@@ -39,12 +39,21 @@ router.delete('/notes/:id', (req, res) => { // <<< :id is established in deleteN
     } else {
         return res.status(404).send("Error: Note not found!")
     }
-    var data = fs.readFileSync('./db/db.json', {encoding: 'utf-8'});
+    // this.delete(removeNote);
+    
+    
+    //var data = fs.readFileSync('./db/db.json', {encoding: 'utf-8'});
+    // console.log(typeof notes);
+    // notes.forEach((note, index) => {
+    //     if (note.id === removeNote) {
+    //         data.notes.splice(index, 1);
+    //     }
+    // });
     
     var json = JSON.parse(data);
-    var savedNotes = json.notes;
+    var savedNotes = json.notes;  //// this code works but it does not permanently delete objects from json
     json.notes = savedNotes.filter((note) => { return note.id !== removeNote});
-    fs.writeFileSync('./db/db.json', JSON.stringify(json));
+    fs.writeFileSync('./db/db.json', JSON.stringify(json, null, 2));
     return res.status(200).send(`Note Deleted!`);
 });
 
