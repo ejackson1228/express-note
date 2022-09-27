d1ab71b7931dc49b30809afc999e308c2e443bb9
ejackson1228 <ejackson1228@gmail.com>
2022-09-27 11:19:35 -0400

    best attempt at delete route finished

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,8 @@
-const { notes } = require('../db/db.json')
+const { notes } = require('../db/db.json');
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
-const { createNewNote, findById, validateNote, deleteNote } = require('../lib/notes')
-const path = require('path');
+const { createNewNote, findById, validateNote } = require('../lib/notes');
 
 
 router.get('/notes', (req, res) => { // get the notes objects as a response
@@ -11,7 +10,7 @@ router.get('/notes', (req, res) => { // get the notes objects as a response
     res.send(rFile);
 })
 
-router.get('/notes/:id', (req, res) => {
+router.get('/notes/:id', (req, res) => { // get a specific object by id as response
     notesArray = fs.readFileSync('./db/db.json');
     const result = findById(req.params.id, notesArray);
     if (result) {
@@ -22,8 +21,8 @@ router.get('/notes/:id', (req, res) => {
 });
 
 router.post('/notes', (req, res) => { //post request for new note
-    fs.readFileSync('./db/db.json');
-    req.body.id = uuidv4();
+    /* fs.readFileSync('./db/db.json', {encoding: 'utf-8'}); */
+    req.body.id = uuidv4(); // randomized id generator
 
     if (!validateNote(req.body))  {
         res.status(400).send("This note is not formatted properly!");
@@ -36,10 +35,10 @@ router.post('/notes', (req, res) => { //post request for new note
 
 router.delete('/notes/:id', (req, res) => { // <<< :id is established in deleteNote() @ index.js line 45
     if (req.params.id) {
-    var removeNote = req.params.id;
-    var data = fs.readFileSync('./db/db.json', {encoding: 'utf-8'});
-    data = notes.filter((note) => { return note.id !== removeNote});
-    fs.writeFileSync('./db/db.json', JSON.stringify({ notes: data }, null, 2));
+    var removeNote = req.params.id; // requested note to remove by id
+    var data = fs.readFileSync('./db/db.json', {encoding: 'utf-8'}); //read current json file
+    data = notes.filter((note) => { return note.id !== removeNote}); // return a new file with every object except the one being removed
+    fs.writeFileSync('./db/db.json', JSON.stringify({ notes: data }, null, 2)); // write this new file to db.json
     return res.status(200).send(`Note Deleted!`);
     } else {
         return res.status(404).send("Error: Note not found!");
